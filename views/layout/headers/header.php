e1ee5037dd1ebf81a8ab098b4741d990efc834a0
kelvin abache <kelvinabache12@gmail.com>
2026-02-06 09:12:20 -0400

    1

--- views/layout/headers/header.php
+++ views/layout/headers/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/styleer.css">
+    <link rel="stylesheet" href="./../../../public/css/styleer.css">
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet"/>
     <title>El Mus Culito</title>
